e11beb543153e3b5c0e44b11113e3a48a671d9e9
Iguanya <georgezmwanchez1996@gmai.com>
2026-01-02 15:05:21 +0300

    Revert "config update"
    This reverts commit 2133b9687dadc24f94c30506288b1403080307da.

--- config/config.php
+++ config/config.php
@@ -1,16 +1,7 @@
-<?php
+<?PHP
+/**
+ * Define Database parameters for PostgreSQL
+ */
 $db_url = parse_url(getenv('DATABASE_URL'));
-
-define('DB_HOST', $db_url['host']);
-define('DB_PORT', $db_url['port'] ?? 3306);
-define('DB_NAME', ltrim($db_url['path'], '/'));
-define('DB_USER', $db_url['user']);
-define('DB_PASS', $db_url['pass']);
-
-define(
-    'DB_DSN',
-    "mysql:host=" . DB_HOST .
-    ";port=" . DB_PORT .
-    ";dbname=" . DB_NAME .
-    ";charset=utf8mb4"
-);
+define('DB_DSN', "pgsql:host=" . $db_url['host'] . ";port=" . ($db_url['port'] ?? 5432) . ";dbname=" . ltrim($db_url['path'], '/') . ";user=" . $db_url['user'] . ";password=" . $db_url['pass']);
+?>
